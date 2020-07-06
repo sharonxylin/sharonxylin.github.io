4b8330b01d13844d5d0046851d79bd0175265c25
Sharon Lin <sl742041@gmail.com>
2020-07-06 20:24:25 +0800

    more files added

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 //projects to display on the dashbaord
 let projects=[];
-projects.push({name: "collewear",tool:"HTML/CSS/JavaScript", relevant:"INFO340 Client-Side Development", cover: "../assets/project_covers/collewear.png", link:"collewear.html"});
-projects.push({name:"SafePath", tool:"Figma", relevant:"WINFO Hackathon", cover:"../assets/project_covers/safepath.png", link:"safepath.html"});
-projects.push({name:"Suicide and Mental Health Disorders", tool:"R", relevant:"INFO 201 Technical Foundations of Informatics", cover:"../assets/project_covers/smhd.png", link:"smhd.html"});
-projects.push({name:"HuskyMap Server", tool:"Java", relevant:"CSE 373 Data Structures & Algorithms", cover:"../assets/project_covers/huskymap.png", link:"huskymap.html"});
-projects.push({name:"EDA Visual Effects Corporation",tool:"Adobe Illustrator/Photoshop",relevant:"Internship", cover:"../assets/project_covers/edavfx.png", link:"edavfx.html"});
+projects.push({name: "collewear",tool:"HTML/CSS/JavaScript", relevant:"INFO340 Client-Side Development", cover: "./src/assets/project_covers/collewear.png", link:"./src/pages/collewear.html"});
+projects.push({name:"SafePath", tool:"Figma", relevant:"WINFO Hackathon", cover:"./src/assets/project_covers/safepath.png", link:"./src/pages/safepath.html"});
+projects.push({name:"Suicide and Mental Health Disorders", tool:"R", relevant:"INFO 201 Technical Foundations of Informatics", cover:"./src/assets/project_covers/smhd.png", link:"./src/pages/smhd.html"});
+projects.push({name:"HuskyMap Server", tool:"Java", relevant:"CSE 373 Data Structures & Algorithms", cover:"./src/assets/project_covers/huskymap.png", link:"./src/pages/huskymap.html"});
+projects.push({name:"EDA Visual Effects Corporation",tool:"Adobe Illustrator/Photoshop",relevant:"Internship ", cover:"./src/assets/project_covers/edavfx.png", link:"./src/pages/edavfx.html"});
 
 
 function createProjects(){
-    let dashboard = document.querySelector('main'); // problem - can't get dashboard
+    let dashboard = document.querySelector('#dashboard'); 
     projects.forEach(project => {
         let projectCard = createProjectCard(project);  
         dashboard.appendChild(projectCard);
@@ -19,15 +19,20 @@ function createProjects(){
 
 function createProjectCard(item) {
     let card = document.createElement('article');
+    card.classList.add('flex-item');
     card.classList.add('card');
     //cover image
     // link to the project page 
-    let name = document.createElement('h2');
+    let name = document.createElement('h3');
     name.innerHTML = item.name.toUpperCase();
     let tool = document.createElement('p');
     tool.innerHTML = item.tool;
     let relevant = document.createElement('p');
     relevant.innerHTML = item.relevant;
+    let image = document.createElement('img');
+    image.setAttribute('src',item.cover);
+
+    //card.appendChild(image); 
     card.appendChild(name);
     card.appendChild(tool);
     card.appendChild(relevant);
