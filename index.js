1cb85ef9238974bbbc18b257c5a20213f18ffd8c
Sharon Lin <sl742041@gmail.com>
2020-09-10 11:32:31 +0800

    where-to-go info added

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 let projects=[];
-projects.push({name:"Where to Go?", tool:"Google Maps Platform", relevant: "Personal Project", cover:"./src/assets/project_covers/wheretogo.png", link:"./src/pages/wheretogo.html"})
-projects.push({name:"Quick Save", tool:"Chrome Extension", relevant:"Personal Project", cover:"./src/assets/project_covers/quicksave.png", link:"./src/pages/quicksave.html"});
 projects.push({name: "collewear",tool:"HTML/CSS/JavaScript", relevant:"INFO340 Client-Side Development", cover: "./src/assets/project_covers/collewear.png", link:"./src/pages/collewear.html"});
+projects.push({name:"Where to Go?", tool:"Google Maps Platform ", relevant: "Personal Project", cover:"./src/assets/project_covers/wheretogo.png", link:"./src/pages/wheretogo.html"})
+projects.push({name:"Quick Save", tool:"Chrome Extension", relevant:"Personal Project", cover:"./src/assets/project_covers/quicksave.png", link:"./src/pages/quicksave.html"});
 projects.push({name:"Hopeful", tool:"Figma/Angular.js", relevant: "Hack'20 Virtual Hackathon", cover:"./src/assets/project_covers/hopeful.png", link:"./src/pages/hopeful.html"});
+projects.push({name:"HuskyMap Server", tool:"Java", relevant:"CSE 373 Data Structures & Algorithms", cover:"./src/assets/project_covers/huskymap.png", link:"./src/pages/huskymap.html"});
 projects.push({name:"SafePath", tool:"Figma", relevant:"WINFO Hackathon", cover:"./src/assets/project_covers/safepath.png", link:"./src/pages/safepath.html"});
 projects.push({name:"Suicide and Mental Health Disorders", tool:"R/Shiny App", relevant:"INFO 201 Technical Foundations of Informatics", cover:"./src/assets/project_covers/smhd.png", link:"./src/pages/smhd.html"});
-projects.push({name:"HuskyMap Server", tool:"Java", relevant:"CSE 373 Data Structures & Algorithms", cover:"./src/assets/project_covers/huskymap.png", link:"./src/pages/huskymap.html"});
 projects.push({name:"EDA Visual Effects Corporation",tool:"Adobe Illustrator/Photoshop",relevant:"Internship ", cover:"./src/assets/project_covers/edavfx.png", link:"./src/pages/edavfx.html"});
 
 // navigation bar interaction 
